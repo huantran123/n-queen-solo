69a8879316d4547a823d70b0b60d9ebb43ffcd91
Huan Tran <huanductran98@gmail.com>
2022-06-28 16:19:09 -0700

    Complete findNQueens

--- src/solvers.js
+++ src/solvers.js
@@ -33,7 +33,14 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-
+  var solution = undefined;
+  if (n === 0) {
+    solution = [];
+  } else if (n === 2 || n === 3) {
+    solution =  makeEmptyMatrix(n);
+  } else {
+    solution = findNQueensSolutionSet(n)[0];
+  }
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
@@ -42,7 +49,13 @@ window.findNQueensSolution = function(n) {
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
   var solutionCount = undefined; //fixme
-
+  if (n === 0) {
+    solutionCount = 1;
+  } else if (n === 2 || n === 3) {
+    solutionCount = 0;
+  } else {
+    solutionCount = findNQueensSolutionSet(n).length;
+  }
 
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
@@ -85,4 +98,34 @@ var findNRooksSolutionSet = function(n) {
   findNRooksSolutions(firstRowIndex);
 
   return solutions;
+}
+
+
+var findNQueensSolutionSet = function(n) {
+  var matrix = makeEmptyMatrix(n);
+  var board = new Board(matrix);
+  var solutions = [];
+  var firstRowIndex = 0;
+
+  var findNQueensSolutions = function(rowIndex) {
+    if (rowIndex === n) {
+      var rows = JSON.parse(JSON.stringify(board.rows()));
+      solutions.push(rows);
+      return;
+    }
+
+    for (var colIndex = 0; colIndex < n; colIndex++) {
+      board.togglePiece(rowIndex, colIndex);
+      if (!board.hasColConflictAt(colIndex) && !board.hasRowConflictAt(rowIndex) && !board.hasAnyMajorDiagonalConflicts() && !board.hasAnyMinorDiagonalConflicts()) {
+        findNQueensSolutions(rowIndex + 1);
+        board.togglePiece(rowIndex, colIndex);
+      } else {
+        board.togglePiece(rowIndex, colIndex);
+      }
+    }
+  }
+
+  findNQueensSolutions(firstRowIndex);
+
+  return solutions
 }
